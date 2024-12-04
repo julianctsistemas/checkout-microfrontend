a68f60401c58a11809ab5d527137dbe2e31d8671
julianctsistemas <julianctsistemas@gmail.com>
2024-12-04 10:44:49 -0500

    update checkout

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
-import 'app.dart';
+import 'package:shop/presentation/screens/checkout_home.dart';
+
 
 void main() {
   runApp(CheckoutApp());
+}
+
+class CheckoutApp extends StatelessWidget {
+  const CheckoutApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false, // Quita el banner DEBUG
+      title: 'Checkout Microfrontend',
+      home: const CheckoutHomeScreen(), // Pantalla principal del microfrontend
+    );
+  }
 }
